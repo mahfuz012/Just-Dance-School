d1c2a695d2f05e4b38be423a7d1af745c164fcc9
mahfuz012 <mahfuzrn012@gmail.com>
2023-06-14 12:48:17 +0600

    firebase setup

--- src/SharedFilesALL/PopularIntructorfiles.jsx
+++ src/SharedFilesALL/PopularIntructorfiles.jsx
@@ -11,7 +11,7 @@ const PopularIntructorfiles = () => {
 
 
     return (
-        <div>
+        <div  className=' p-5  parent-instrucotr '>
             {
               popularclass.map(p=><CardInstrcutor key={p._id} data={p} />)  
             }
@@ -22,7 +22,32 @@ const PopularIntructorfiles = () => {
 
 
 function CardInstrcutor({data}){
-const {instructorName,email,instructorImage} = data
+const {instructorName,email,instructorImage,className,enroll} = data
+
+
+
+return (
+    <>
+    <div className='sm:w-6/12 m-1 shadow-2xl rounded-br-3xl  sm:flex border-4 border-violet-700 rounded-tr-3xl parent-instrucotr'>
+        <div className='sm:w-8/12 h-[15rem]  '>
+            <img className='w-full h-full ' src={instructorImage} />
+        </div>
+
+        <div style={{backgroundColor:"#FEFF86",color:"#146C94"}} className='sm:w-full  rounded-tr-2xl rounded-br-2xl'>
+       <p className='font-semibold m-5  text-center text-5xl'>{instructorName}</p>
+       <p className='font-semibold m-5  text-2xl'>Total Enroll :{enroll}</p>
+       <p className='font-semibold m-5  text-xl'>Gmail : {email}</p>
+       <p className='font-semibold m-5  text-xl'>Class Name : {className}</p>
+   
+        </div>
+
+    </div>
+    
+    
+    </>
+)
+
+
 }
 
 
