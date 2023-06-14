90058680e4b1c48482a657f401024cffb6e4a107
mahfuz012 <mahfuzrn012@gmail.com>
2023-06-14 14:27:13 +0600

    private route setup

--- src/SharedFilesALL/PopularIntructorfiles.jsx
+++ src/SharedFilesALL/PopularIntructorfiles.jsx
@@ -11,7 +11,7 @@ const PopularIntructorfiles = () => {
 
 
     return (
-        <div  className=' p-5  parent-instrucotr '>
+        <div  className=' px-10 py-5 my-10  '>
             {
               popularclass.map(p=><CardInstrcutor key={p._id} data={p} />)  
             }
@@ -28,12 +28,12 @@ const {instructorName,email,instructorImage,className,enroll} = data
 
 return (
     <>
-    <div className='sm:w-6/12 m-1 shadow-2xl rounded-br-3xl  sm:flex border-4 border-violet-700 rounded-tr-3xl parent-instrucotr'>
-        <div className='sm:w-8/12 h-[15rem]  '>
+    <div className=' sm:w-6/12 m-1 shadow-2xl sm:rounded-br-3xl  sm:flex border-4 border-violet-700 sm:rounded-tr-3xl parent-instrucotr perpective3d'>
+        <div className='sm:w-8/12 h-[16rem]  '>
             <img className='w-full h-full ' src={instructorImage} />
         </div>
 
-        <div style={{backgroundColor:"#FEFF86",color:"#146C94"}} className='sm:w-full  rounded-tr-2xl rounded-br-2xl'>
+        <div style={{backgroundColor:"#F45050",color:"#F0F0F0"}} className='w-full p-1 rounded-tr-2xl rounded-br-2xl'>
        <p className='font-semibold m-5  text-center text-5xl'>{instructorName}</p>
        <p className='font-semibold m-5  text-2xl'>Total Enroll :{enroll}</p>
        <p className='font-semibold m-5  text-xl'>Gmail : {email}</p>
